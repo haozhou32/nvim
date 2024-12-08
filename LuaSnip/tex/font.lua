3ec6eb951fa14236027dc8ef2ee302d9831b1e40
haozhou32 <zhouhao199102@gmail.com>
2024-12-08 21:12:46 +0800

    change some snippets

--- LuaSnip/tex/font.lua
+++ LuaSnip/tex/font.lua
@@ -12,36 +12,36 @@ local rep = require("luasnip.extras").rep
 -- When `LS_SELECT_RAW` is empty, the function simply returns an empty insert node.
 
 local get_visual = function(args, parent)
-  if (#parent.snippet.env.LS_SELECT_RAW > 0) then
+  if #parent.snippet.env.LS_SELECT_RAW > 0 then
     return sn(nil, i(1, parent.snippet.env.LS_SELECT_RAW))
-  else  -- If LS_SELECT_RAW is empty, return a blank insert node
+  else -- If LS_SELECT_RAW is empty, return a blank insert node
     return sn(nil, i(1))
   end
 end
 
 local tex_utils = {}
-tex_utils.in_mathzone = function()  -- math context detection
-  return vim.fn['vimtex#syntax#in_mathzone']() == 1
+tex_utils.in_mathzone = function() -- math context detection
+  return vim.fn["vimtex#syntax#in_mathzone"]() == 1
 end
 tex_utils.in_text = function()
   return not tex_utils.in_mathzone()
 end
-tex_utils.in_comment = function()  -- comment detection
-  return vim.fn['vimtex#syntax#in_comment']() == 1
+tex_utils.in_comment = function() -- comment detection
+  return vim.fn["vimtex#syntax#in_comment"]() == 1
 end
-tex_utils.in_env = function(name)  -- generic environment detection
-    local is_inside = vim.fn['vimtex#env#is_inside'](name)
-    return (is_inside[1] > 0 and is_inside[2] > 0)
+tex_utils.in_env = function(name) -- generic environment detection
+  local is_inside = vim.fn["vimtex#env#is_inside"](name)
+  return (is_inside[1] > 0 and is_inside[2] > 0)
 end
 -- A few concrete environments---adapt as needed
-tex_utils.in_equation = function()  -- equation environment detection
-    return tex_utils.in_env('equation')
+tex_utils.in_equation = function() -- equation environment detection
+  return tex_utils.in_env("equation")
 end
-tex_utils.in_itemize = function()  -- itemize environment detection
-    return tex_utils.in_env('itemize')
+tex_utils.in_itemize = function() -- itemize environment detection
+  return tex_utils.in_env("itemize")
 end
-tex_utils.in_tikz = function()  -- TikZ picture environment detection
-    return tex_utils.in_env('tikzpicture')
+tex_utils.in_tikz = function() -- TikZ picture environment detection
+  return tex_utils.in_env("tikzpicture")
 end
 
 --
@@ -49,77 +49,63 @@ end
 --
 
 return {
-s({trig="tbf", snippetType = "autosnippet", dscr="Expands 'tbf' into '\textbf{}'"},
-  fmta("\\textbf{<>}",
-      {
-        d(1, get_visual),
-      }
-    )
-),
-
-s({trig="tit", snippetType = "autosnippet", dscr="Expands 'tit' into '\textit{}'"},
-  fmta("\\textit{<>}",
-      {
-        d(1, get_visual),
-      }
-    )
-),
-
-
-s({trig="ovl", snippetType = "autosnippet", dscr="Expands 'ovl' into '\\overline{}'"},
-  fmta("\\overline{<>}",
-      {
-        d(1, get_visual),
-      }
-    ),
-  { condition = tex_utils.in_mathzone }
-),
-
-s({trig="udl", snippetType = "autosnippet", dscr="Expands 'udl' into '\\underline{}'"},
-  fmta("\\underline{<>}",
-      {
-        d(1, get_visual),
-      }
-    ),
-  { condition = tex_utils.in_mathzone }
-),
-
-s({trig="blue", dscr="Expands 'blue' into '{\\color{blue} <>}'"},
-  fmta("{\\color{blue} <>}",
-      {
-        d(1, get_visual),
-      }
-    )
-),
-
-s({trig="red", dscr="Expands 'red' into '{\\color{red} <>}'"},
-  fmta("{\\color{red} <>}",
-      {
-        d(1, get_visual),
-      }
-    )
-),
-
-s(
-    { trig = "cal", snippetType = "autosnippet" },
-    fmta("\\mathcal{<>}",
-       {
-       d(1, get_visual),
-       }
-      ),
-    { condition = tex_utils.in_mathzone }
- ),
-
-s(
-    { trig = "mbb", snippetType = "autosnippet" },
-    fmta("\\mathbb{<>}",
-      {
+  s(
+    { trig = "tbf", snippetType = "autosnippet", dscr = "Expands 'tbf' into '\textbf{}'" },
+    fmta("\\textbf{<>}", {
       d(1, get_visual),
-      }
-      ),
+    })
+  ),
+
+  s(
+    { trig = "tit", snippetType = "autosnippet", dscr = "Expands 'tit' into '\textit{}'" },
+    fmta("\\textit{<>}", {
+      d(1, get_visual),
+    })
+  ),
+
+  s(
+    { trig = "ovl", snippetType = "autosnippet", dscr = "Expands 'ovl' into '\\overline{}'" },
+    fmta("\\overline{<>}", {
+      d(1, get_visual),
+    }),
     { condition = tex_utils.in_mathzone }
- ),
+  ),
 
+  s(
+    { trig = "udl", snippetType = "autosnippet", dscr = "Expands 'udl' into '\\underline{}'" },
+    fmta("\\underline{<>}", {
+      d(1, get_visual),
+    }),
+    { condition = tex_utils.in_mathzone }
+  ),
 
+  s(
+    { trig = "blue", dscr = "Expands 'blue' into '{\\color{blue} <>}'" },
+    fmta("{\\color{blue} <>}", {
+      d(1, get_visual),
+    })
+  ),
 
+  s(
+    { trig = "red", dscr = "Expands 'red' into '{\\color{red} <>}'" },
+    fmta("{\\color{red} <>}", {
+      d(1, get_visual),
+    })
+  ),
+
+  s(
+    { trig = "mc", snippetType = "autosnippet" },
+    fmta("\\mathcal{<>}", {
+      d(1, get_visual),
+    }),
+    { condition = tex_utils.in_mathzone }
+  ),
+
+  s(
+    { trig = "mb", snippetType = "autosnippet" },
+    fmta("\\mathbb{<>}", {
+      d(1, get_visual),
+    }),
+    { condition = tex_utils.in_mathzone }
+  ),
 }
