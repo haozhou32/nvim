34f01ea64ff9775b12b64b89b522a2646bb6edbf
haozhou32 <zhouhao199102@gmail.com>
2025-06-11 16:21:58 +0800

    update

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -85,13 +85,13 @@ return {
       -- Set menu
       dashboard.section.buttons.val = {
         dashboard.button("e", "  > New file", ":ene <BAR> startinsert <CR>"),
-        dashboard.button("f", "  > Find file", ":cd $HOME | Telescope find_files<CR>"),
+        dashboard.button("f", "󰈞  > Find file", ":cd $HOME | Telescope find_files<CR>"),
         dashboard.button("r", "  > Recent", ":Telescope oldfiles<CR>"),
         dashboard.button("c", "  > Config", ":e $MYVIMRC | :cd %:p:h | split . | wincmd k | pwd<CR>"),
-        dashboard.button("i", "  Info", "<cmd>e ~/.config/nvim/README.md<cr>"),
-        dashboard.button("p", "  Plugins", "<cmd>Lazy<cr>"),
-        dashboard.button("h", "  Checkhealth", "<cmd>checkhealth<cr>"),
-        dashboard.button("q", "  > Quit NVIM", ":qa<CR>"),
+        dashboard.button("i", "  > Info", "<cmd>e ~/.config/nvim/README.md<cr>"),
+        dashboard.button("p", "  > Plugins", "<cmd>Lazy<cr>"),
+        dashboard.button("h", "  > Checkhealth", "<cmd>checkhealth<cr>"),
+        dashboard.button("q", "󰩈  > Quit NVIM", ":qa<CR>"),
       }
 
       -- Send config to alpha
